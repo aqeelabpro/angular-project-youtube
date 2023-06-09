e6c77ece1baaf315150b1052f7c82c5bee5ac50e
aqeelabpro <93031839+aqeelabpro@users.noreply.github.com>
2023-06-10 00:30:03 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-project-youtube
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-welcome-app-hcsy9e)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+  today = new Date();
+  text = 'hello world';
+  lowerCaseText = 'HELLO WORLD';
+  person = {
+    name: 'John doe',
+    age: 12,
+    email: 'john@gmail.com',
+  };
+
+  saveData(userdata) {
+    console.log(userdata);
+  }
+
+  saveUserData(userData) {
+    console.log(userData);
+  }
+}
