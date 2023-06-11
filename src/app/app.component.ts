e677d7997377a1435c873016049280fee1486758
aqeelabpro <93031839+aqeelabpro@users.noreply.github.com>
2023-06-11 09:19:35 +0500

    use reactive forms to handle user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+    });
+  }
+
+  saveTheReactiveFormData(formData) {
+    console.log(formData);
+  }
+
+  formGroup: FormGroup;
+
   name = 'Angular';
   today = new Date();
   text = 'hello world';
